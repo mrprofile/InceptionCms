bacd4dd3458f7a6cafda7cebf7255a421dbb0b56
unknown <501936093@LA1-92KK942.tfayd.com>
2015-05-14 17:19:46 -0700

    added ngConfig class

---
+++ Web/Infrastructure/MvcViewPage/NgConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Inception.Web.Infrastructure.MvcViewPage
+{
+    public class NgConfig
+    {
+        public string NgApp { get; set; }
+        public string NgController { get; set; }
+    }
+}
