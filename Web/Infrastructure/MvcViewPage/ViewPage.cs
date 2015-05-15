867b8fc9143b588e7cc86cc20e48ec7f2d53ce4f
unknown <501936093@LA1-92KK942.tfayd.com>
2015-05-14 17:37:19 -0700

    added few classes

--- Web/Infrastructure/MvcViewPage/ViewPage.cs
+++ Web/Infrastructure/MvcViewPage/ViewPage.cs
@@ -4,5 +4,21 @@ namespace Inception.Web.Infrastructure.MvcViewPage
 {
     public abstract class WebViewPage<TModel> : System.Web.Mvc.WebViewPage<TModel> where TModel : BasePageModel
     {
+        private NgConfig _ngConfig;
+        public NgConfig NgConfig
+        {
+            get { return _ngConfig ?? (_ngConfig = new NgConfig()); }
+            set { _ngConfig = value; }
+        }
+
+        protected override void InitializePage()
+        {
+            base.InitializePage();
+
+            if (Model != null)
+            {
+                _ngConfig = Model.NgConfig;
+            }
+        }
     }
 }
--- Web/Models/BasePageModel.cs
+++ Web/Models/BasePageModel.cs
@@ -1,9 +1,28 @@
-﻿namespace Inception.Web.Models
+﻿using Inception.Web.Infrastructure.MvcViewPage;
+
+namespace Inception.Web.Models
 {
     public class BasePageModel
-    {
+    {        
         public string Title { get; set; }
         public string MetaDescription { get; set; }
         public string MetaKeywords { get; set; }
+
+        private NgConfig _ngConfig;
+        public NgConfig NgConfig
+        {
+            get { return _ngConfig ?? (_ngConfig = new NgConfig()); }
+            set { _ngConfig = value; }
+        }
+
+        public BasePageModel()
+        {
+        }
+
+        public BasePageModel(NgConfig config)
+            : this()
+        {
+            _ngConfig = config;
+        }
     }
 }
