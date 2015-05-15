cc9260b6dc12e045610bcbb51973288e92cab7e8
unknown <501936093@LA1-92KK942.tfayd.com>
2015-05-14 17:43:17 -0700

    added ngConfig in controller

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using Inception.Web.Infrastructure.MvcViewPage;
 
 namespace Inception.Web.Controllers
 {
@@ -6,28 +7,28 @@ namespace Inception.Web.Controllers
     {
         public ActionResult Index()
         {
-            var viewModel = new Models.Home.Index  { Title = "Home Page of Inception" };
+            var viewModel = new Models.Home.Index { Title = "Home Page of Inception" };
 
             return View(viewModel);
         }
 
         public ActionResult Videos()
         {
-            var viewModel = new Models.Video.Index { Title = "Home Page of Inception" };
+            var viewModel = new Models.Video.Index { Title = "Home Page of Inception", NgConfig = new NgConfig() { NgApp = "video", NgController = "VideoCntrl as vm" } };
 
             return View(viewModel);
         }
 
         public ActionResult Sliders()
         {
-            var viewModel = new Models.Slider.Index() { Title = "Home Page of Inception" };
+            var viewModel = new Models.Slider.Index() { Title = "Home Page of Inception", NgConfig = new NgConfig() { NgApp = "slider", NgController = "SliderCntrl as vm" } };
 
             return View(viewModel);
         }
 
         public ActionResult Themes()
         {
-            var viewModel = new Models.Theme.Index { Title = "Theme Page of Inception" };
+            var viewModel = new Models.Theme.Index { Title = "Theme Page of Inception", NgConfig = new NgConfig() { NgApp = "theme", NgController = "ThemeCntrl as vm" } };
 
             return View(viewModel);
         }
