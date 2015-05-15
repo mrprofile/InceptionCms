80dd4c1cf624af01a54975182ebde5dc626f4c8e
unknown <501936093@LA1-92KK942.tfayd.com>
2015-05-15 11:27:57 -0700

    integrated angularJs into views

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -14,21 +14,30 @@ namespace Inception.Web.Controllers
 
         public ActionResult Videos()
         {
-            var viewModel = new Models.Video.Index { Title = "Home Page of Inception", NgConfig = new NgConfig() { NgApp = "video", NgController = "VideoCntrl as vm" } };
+            var viewModel = new Models.Video.Index
+            {
+                Title = "Home Page of Inception",
+            };
 
             return View(viewModel);
         }
 
         public ActionResult Sliders()
         {
-            var viewModel = new Models.Slider.Index() { Title = "Home Page of Inception", NgConfig = new NgConfig() { NgApp = "slider", NgController = "SliderCntrl as vm" } };
+            var viewModel = new Models.Slider.Index()
+            {
+                Title = "Home Page of Inception"
+            };
 
             return View(viewModel);
         }
 
         public ActionResult Themes()
         {
-            var viewModel = new Models.Theme.Index { Title = "Theme Page of Inception", NgConfig = new NgConfig() { NgApp = "theme", NgController = "ThemeCntrl as vm" } };
+            var viewModel = new Models.Theme.Index
+            {
+                Title = "Theme Page of Inception"
+            };
 
             return View(viewModel);
         }
