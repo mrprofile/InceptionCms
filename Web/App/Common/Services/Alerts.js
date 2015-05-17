cbfb3c5a439b03ec46bfa8dc582a85928f99f14d
mrprofile <ccapil@yahoo.com>
2015-05-17 03:06:04 -0700

    Added Alert service

---
+++ Web/App/Common/Services/Alerts.js
@@ -0,0 +1,57 @@
+﻿(function () {
+    'use strict';
+    var commonModule = angular.module('esqtv.common');
+
+    commonModule.factory('AlertService', [ '$timeout', alertService]);
+
+    function alertService($timeout) {
+        var currentAlerts = [];
+        var alertTypes = ['success', 'warning', 'info', 'danger'];
+        
+        var alerts = {
+            addWarning: addWarning,
+            addSuccess: addSuccess,
+            addInfo: addInfo,
+            addDanger: addDanger,
+            addAlert : addAlert,
+            currentAlerts: currentAlerts,
+            alertTypes: alertTypes
+        };
+       
+        return alerts;
+        
+        function addWarning(message) {
+            addAlert('warning', message);
+        }
+        
+        function addSuccess(message) {
+            addAlert('success', message);
+        }
+        
+        function addInfo(message) {
+            addAlert('info', message);
+        }
+        
+        function addDanger(message) {
+            addAlert('danger', message);
+        }
+        
+        function addAlert(type, message) {
+            var alert = { type: type, msg: message };
+            
+            currentAlerts.push(alert);
+
+            $timeout(function () {
+                removeAlert(alert);
+            }, 3500);
+        }
+        
+        function removeAlert(alert) {
+            for (var i = 0; i < currentAlerts.length; i++) {
+                if (currentAlerts[i] == alert) {
+                    currentAlerts.splice(i, 1);
+                }
+            }
+        }
+    }
+})();
--- Web/App/Video/Controllers/VideoIndexCntrl.js
+++ Web/App/Video/Controllers/VideoIndexCntrl.js
@@ -1,13 +1,20 @@
 ﻿(function() {
     'use strict';
 
-    angular.module('esqtv.video').controller("VideoIndexCntrl", ['$scope', '$http', '$q', '$routeParams', '$window', '$location', 'VideoService', videoCntrl]);
+    angular.module('esqtv.video').controller("VideoIndexCntrl", ['$scope', '$http', '$q', '$routeParams', '$window', '$location', 'VideoService', 'AlertService', videoCntrl]);
     
-    function videoCntrl($scope, $http, $q, $routeParams, $window, $location, VideoService) {
+    function videoCntrl($scope, $http, $q, $routeParams, $window, $location, VideoService, AlertService) {
         var vm = this;
+        
         vm.videos = [];
+        vm.alerts = AlertService.currentAlerts;
         vm.selectedVideo = {};
+        vm.message = '';
         activate();
+        
+        vm.addAlert = function () {
+            AlertService.addSuccess(vm.message);
+        };
 
         function activate() {           
         }
