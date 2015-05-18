8095e8081b9f7b7bb5bd50714c74b3447d2510f4
mrprofile <lakers817>
2015-05-18 14:24:02 -0700

    add requestCountInterceptor file

---
+++ Web/App/Common/Interceptors/RequestCountInterceptor.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+    var commonModule = angular.module('esqtv.common');
+
+    commonModule.factory('requestCountInterceptor', ['$q', requestCountInterceptor]);
+    
+    function requestCountInterceptor($q) {
+        var requestCount = 0;
+        
+        var request = {
+
+            request: function (config) {
+                requestCount += 1;
+
+                return $q.when(config);
+            },
+            requestError: function (error) {
+                requestCount -= 1;
+
+                return $q.reject(error);
+            },
+            response: function (response) {
+                requestCount -= 1;
+
+                return $q.when(response);
+            },
+            responseError: function (error) {
+                requestCount -= 1;
+
+                return $q.reject(error);
+            },
+            getRequestCount: function() {
+                return requestCount;
+            }
+        };
+        
+        return request;
+    }
+})();
