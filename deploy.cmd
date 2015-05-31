81767a4d043ecfdf76a221da6ec8052549bcc369
Christian capil <christian.capil@gmail.com>
2015-05-30 20:49:04 -0700

    Fix incorrect spelling for bower.json

--- deploy.cmd
+++ deploy.cmd
@@ -89,7 +89,7 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 )
 
 :: 4. Bower Install
-if EXIST "%DEPLOYMENT_TARGET%\Wbower.json" (
+if EXIST "%DEPLOYMENT_TARGET%\bower.json" (
     pushd "%DEPLOYMENT_TARGET%"
     call :ExecuteCmd bower install
     IF !ERRORLEVEL! NEQ 0 goto error
