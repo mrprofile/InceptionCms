cd83273b776d12b7eb64afb07c880e13a79a5925
mrprofile <ccapil@yahoo.com>
2015-05-31 11:30:12 -0700

    added enterKey directive

--- Web/App/Common/Directives/Helpers.js
+++ Web/App/Common/Directives/Helpers.js
@@ -12,10 +12,30 @@
             template: '<md-list-item layout="row">'+
                         '<md-input-container>'+
                             '<label>{{labeltext}}</label>' +
-                            '<input type="text" data-ng-model="query" />' +
+                            '<input type="text" data-ng-model="query" ent-enter action="search()" />' +
                         '</md-input-container>'+
                         '<md-button ng-click="search()" class="btn btn-primary"><i class="glyphicon glyphicon-search">{{buttontext}}</md-button>' +
                       '</md-list-item>'
         };
     });
+    
+    angular.module('esqtv.common').directive('entEnter', function () {
+        return {
+            restrict: 'A',
+            scope: {
+                action: "&action"
+            },
+            link: function (scope, elements) {
+                elements.bind('keydown keypress', function (event) {
+                    if (event.which === 13) {
+                        scope.$apply(function () {
+                            scope.$eval(scope.action());
+                        });
+                        event.preventDefault();
+                    }
+                });
+            }
+        };  
+    });
+
 })();
