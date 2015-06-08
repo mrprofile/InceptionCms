87ae7272631361b762fdcbebfe8ec7866035b64f
mrprofile <lakers817>
2015-06-08 16:56:58 -0700

    Add Theme page modules, controllers, views, services

---
+++ Web/App/Theme/Config.js
@@ -0,0 +1,16 @@
+﻿;(function () {
+    'use strict';
+
+    angular.module('esqtv.theme').config(function ($routeProvider) {
+        $routeProvider
+            .when('/themes', { templateUrl: '/App/Theme/Views/Index.html', controller: 'ThemeIndexCntrl', controllerAs: 'vm' })
+            .when('/themes/edit/:id', {
+                templateUrl: '/App/Video/Views/Edit.html', controller: 'ThemeEditCntrl', controllerAs: 'vm'
+            });
+        
+    });
+
+    angular.module('esqtv.theme').config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.interceptors.push('addTokenInterceptor');
+    }]);
+})();
---
+++ Web/App/Theme/Controllers/ThemeIndexCntrl.js
@@ -0,0 +1,42 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('esqtv.theme').controller("ThemeIndexCntrl", ['SearchService', 'ThemeService', themeCntrl]);
+    
+    function themeCntrl(searchService, themeService) {
+        var vm = this;
+
+        vm.ss = searchService;
+        vm.ss.paging.onSelectPage = searchThemes;
+        vm.searchThemes = function () {
+            vm.ss.paging.currentPage = 1;
+            searchThemes();
+        };
+        vm.selectItem = selectedItem;
+
+        activate();
+        
+        function activate() {
+            searchThemes();
+        }
+        
+        function searchThemes() {
+            themeService.search(vm.ss.search, vm.ss.paging, searchThemesCompleted, searchThemesError);
+        }
+
+        function searchThemesCompleted(response) {
+            console.log(response);
+            vm.ss.search.results = response.result;
+            vm.ss.paging.totalItems = response.totalItems;
+        }
+
+        function searchThemesError(response) {
+            //TODO: Call Notify Service
+        }
+        
+        function selectedItem(item) {
+            console.log(item);
+            alert('redirect to edit page');
+        }
+    };
+})();
