09289a714523a8533fea47b64ac5636a8376363d
mrprofile <ccapil@yahoo.com>
2015-06-08 18:58:57 -0700

    remove record in grid after successful delete

--- Web/App/Theme/Controllers/ThemeIndexCntrl.js
+++ Web/App/Theme/Controllers/ThemeIndexCntrl.js
@@ -7,6 +7,7 @@
         var vm = this;
 
         vm.ss = searchService;
+        vm.deleteItem = deleteItem;
         vm.ss.paging.onSelectPage = searchThemes;
         vm.searchThemes = function () {
             vm.ss.paging.currentPage = 1;
@@ -27,6 +28,16 @@
         function searchThemesCompleted(response) {
             vm.ss.search.results = response.result;
             vm.ss.paging.totalItems = response.totalItems;
+        }
+        
+        function deleteItem(id) {
+            var count = 0;
+            vm.ss.search.results.forEach(function (item) {
+                if (item["id"] === id) {
+                    vm.ss.search.results.splice(count, 1);
+                    count++;
+                }
+            });
         }
 
         function searchThemesError() {
